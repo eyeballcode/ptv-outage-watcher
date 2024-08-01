9c4ccd449cadbbf8b0406c911b53167d6ef4d149
Eyeball Edward <edwardyeung39@gmail.com>
2024-08-01 11:10:57 +1000

    check for missing ETA and vehicle data

--- test/test-ptv-metro-detector.test.mjs
+++ test/test-ptv-metro-detector.test.mjs
@@ -21,11 +21,37 @@ describe('The PTV Metro Outage detector', () => {
     expect(status.status).to.equal('Healthy')
   })
 
-  it('Should return a healthy response when the data is normal', async () => {
+  it('Should return an unhealthy response when the op timetable wasn\'t loaded and is using the raw GTFS timetables', async () => {
     nock('https://timetableapi.ptv.vic.gov.au').get(/\/v3\/departures\/route_type\/0\/.+/).reply(200, stubNoOpTimetable)
 
     let status = await checkPTVMetro()
     expect(status.status).to.equal('Unhealthy')
     expect(status.code).to.equal('NO_OP_TIMETABLE')
+  })
+
+  it('Should return an unhealthy response when the op timetable is loaded but no ETA available', async () => {
+    let timetable = JSON.parse(JSON.stringify(stubRegularDepartures)) // Lazy hack to clone data
+    timetable.departures.forEach(departure => departure.estimated_departure_utc = null)
+
+    nock('https://timetableapi.ptv.vic.gov.au').get(/\/v3\/departures\/route_type\/0\/.+/).reply(200, timetable)
+
+    let status = await checkPTVMetro()
+    expect(status.status).to.equal('Unhealthy')
+    expect(status.code).to.equal('NO_LIVE_ETA')
+    expect(status.trackingAvailable).to.be.true
+  })
+
+  it('Should return an unhealthy response when the op timetable is loaded with ETAs but no position/fleet data', async () => {
+    let timetable = JSON.parse(JSON.stringify(stubRegularDepartures)) // Lazy hack to clone data
+    timetable.departures.forEach(departure => {
+      timetable.runs[departure.run_ref].vehicle_position = null
+      timetable.runs[departure.run_ref].vehicle_descriptor = null
+    })
+
+    nock('https://timetableapi.ptv.vic.gov.au').get(/\/v3\/departures\/route_type\/0\/.+/).reply(200, timetable)
+
+    let status = await checkPTVMetro()
+    expect(status.status).to.equal('Unhealthy')
+    expect(status.code).to.equal('NO_TRACKING')
   })
 })
