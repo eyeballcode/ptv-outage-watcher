0fb3b19851587c92c006f611c414c0b993b8733f
Eyeball Edward <edwardyeung39@gmail.com>
2024-08-01 11:46:24 +1000

    update run

--- run.mjs
+++ run.mjs
@@ -1,4 +1,6 @@
 import { default as checkPTVMetro } from './lib/check-ptv-metro.mjs'
+import { default as checkPTVBus } from './lib/check-ptv-bus.mjs'
+
 import { PTVAPI, PTVAPIInterface } from 'ptv-api'
 import config from './config.json' assert { type: 'json' }
 
@@ -9,4 +11,11 @@ if (ptvMetro.status === 'Healthy') console.log('PTV Metro: Healthy')
 else {
   console.log(`PTV Metro: Unhealthy (${ptvMetro.code})`)
   if ('trackingUnavailable' in ptvMetro) console.log(`PTV Metro: Extended Tracking: ${ptvMetro.trackingUnavailable ? 'Unavailable' : 'Available'}`)
+}
+
+let ptvBus = await checkPTVMetro(ptvAPI)
+if (ptvBus.status === 'Healthy') console.log('PTV Bus: Healthy')
+else {
+  console.log(`PTV Bus: Unhealthy (${ptvBus.code})`)
+  if ('trackingUnavailable' in ptvBus) console.log(`PTV Bus: Extended Tracking: ${ptvMetro.trackingUnavailable ? 'Unavailable' : 'Available'}`)
 }
