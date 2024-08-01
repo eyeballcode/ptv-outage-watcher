4539abce7a1ba4b5bd8f35b68c9cc98e6df64d4a
Eyeball Edward <edwardyeung39@gmail.com>
2024-08-01 10:48:47 +1000

    set up basic test

---
+++ test/test-ptv-metro-detector.test.mjs
@@ -0,0 +1,22 @@
+import { expect } from 'chai'
+import fs from 'fs/promises'
+import path from 'path'
+import url from 'url'
+import nock from 'nock'
+import { default as checkPTVMetro } from '../lib/check-ptv-metro.mjs'
+
+import stubRegularDepartures from './mock/regular-metro-departures.json' assert { type: 'json' }
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// const gtfsr = (await fs.readFile(path.join(__dirname, 'mock', 'gtfsr-blank-response'))).toString()
+
+describe('The PTV Metro Outage detector', () => {
+  it('Should return a healthy response when the data is normal', async () => {
+    nock('https://timetableapi.ptv.vic.gov.au').get(/\/v3\/departures\/route_type\/0\/.+/).reply(200, stubRegularDepartures)
+
+    let status = await checkPTVMetro()
+    expect(status.status).to.equal('Healthy')
+  })
+})
