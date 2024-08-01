118ffe4d6eae082c2ff6953ba307b114b73ef123
Eyeball Edward <edwardyeung39@gmail.com>
2024-08-01 12:45:11 +1000

    check multiple stops in case departures are unavailable from some

--- run.mjs
+++ run.mjs
@@ -6,14 +6,42 @@ import config from './config.json' assert { type: 'json' }
 
 let ptvAPI = new PTVAPI(new PTVAPIInterface(config.devID, config.key))
 
-let ptvMetro = await checkPTVMetro(ptvAPI)
+let metroCheckStops = [
+  19902, // RWD
+  19915, // CLA
+  20025, // FSY
+  19979, // JLI
+  19854, // FSS
+  19973, // NME
+]
+
+let busCheckStops = [
+  47252, // RWD
+  19810, // Monash
+  47764, // FSY
+  47160, // Northland
+  21477, // BMS
+]
+
+
+let ptvMetro
+for (let stop of metroCheckStops) {
+  ptvMetro = await checkPTVMetro(ptvAPI, stop)
+  if (ptvMetro.status === 'Healthy') break
+}
+
 if (ptvMetro.status === 'Healthy') console.log('PTV Metro: Healthy')
 else {
   console.log(`PTV Metro: Unhealthy (${ptvMetro.code})`)
   if ('trackingUnavailable' in ptvMetro) console.log(`PTV Metro: Extended Tracking: ${ptvMetro.trackingUnavailable ? 'Unavailable' : 'Available'}`)
 }
 
-let ptvBus = await checkPTVMetro(ptvAPI)
+let ptvBus
+for (let stop of busCheckStops) {
+  ptvBus = await checkPTVBus(ptvAPI, stop)
+  if (ptvBus.status === 'Healthy') break
+}
+
 if (ptvBus.status === 'Healthy') console.log('PTV Bus: Healthy')
 else {
   console.log(`PTV Bus: Unhealthy (${ptvBus.code})`)
