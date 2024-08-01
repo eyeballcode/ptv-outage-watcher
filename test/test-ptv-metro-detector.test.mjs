4c311be3e2520e6ae7dad5f11b78ec48bf52f2bb
Eyeball Edward <edwardyeung39@gmail.com>
2024-08-01 11:00:21 +1000

    check for op timetable present first

--- test/test-ptv-metro-detector.test.mjs
+++ test/test-ptv-metro-detector.test.mjs
@@ -6,6 +6,7 @@ import nock from 'nock'
 import { default as checkPTVMetro } from '../lib/check-ptv-metro.mjs'
 
 import stubRegularDepartures from './mock/regular-metro-departures.json' assert { type: 'json' }
+import stubNoOpTimetable from './mock/metro-no-op-timetable.json' assert { type: 'json' }
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -18,5 +19,13 @@ describe('The PTV Metro Outage detector', () => {
 
     let status = await checkPTVMetro()
     expect(status.status).to.equal('Healthy')
+  })
+
+  it('Should return a healthy response when the data is normal', async () => {
+    nock('https://timetableapi.ptv.vic.gov.au').get(/\/v3\/departures\/route_type\/0\/.+/).reply(200, stubNoOpTimetable)
+
+    let status = await checkPTVMetro()
+    expect(status.status).to.equal('Unhealthy')
+    expect(status.code).to.equal('NO_OP_TIMETABLE')
   })
 })
