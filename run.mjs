8fd2d1393b161f02245256c95289fb3c1b6a8ef2
Eyeball Edward <edwardyeung39@gmail.com>
2024-08-01 11:32:05 +1000

    create sample run file

--- run.mjs
+++ run.mjs
@@ -1,4 +1,8 @@
-import fetchTown from './lib/fetch-page.mjs'
+import { default as checkPTVMetro } from './lib/check-ptv-metro.mjs'
 
-let daylesford = await fetchTown('mount buller')
-console.log(daylesford)
+let ptvMetro = await checkPTVMetro()
+if (ptvMetro.status === 'Healthy') console.log('PTV Metro: Healthy')
+else {
+  console.log(`PTV Metro: Unhealthy (${ptvMetro.code})`)
+  if (trackingUnavailable in ptvMetro) console.log(`PTV Metro: Extended Tracking: ${ptvMetro.trackingUnavailable ? 'Unavailable' : 'Available'}`)
+}
